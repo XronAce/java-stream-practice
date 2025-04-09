1ff508a4d6a41074f89c83184f402ec9ef8fa6b7
Donggi Yang <xronace@gmail.com>
2025-04-09 18:58:08 +0900

    feat: Add Problem42 solution

--- src/main/java/problem/medium/Problem42.java
+++ src/main/java/problem/medium/Problem42.java
@@ -12,7 +12,10 @@ public class Problem42 {
      * @return 조건을 만족하는 숫자들의 합
      */
     public static int sumSpecialMultiples(List<Integer> numbers) {
-        // 여기에 코드 작성
-        return 0;
+        return numbers.stream()
+            .filter(n -> n % 3 == 0 || n % 5 == 0)
+            .map(n -> n % 5 == 0 ? n * 2 : n)
+            .mapToInt(Integer::intValue)
+            .sum();
     }
 }
