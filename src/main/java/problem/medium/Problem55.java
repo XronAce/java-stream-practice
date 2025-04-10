bf2c11575e7d6d975e07c6405ecab0b1a2e30b97
Donggi Yang <xronace@gmail.com>
2025-04-10 18:00:22 +0900

    feat: Add Problem55 solution

--- src/main/java/problem/medium/Problem55.java
+++ src/main/java/problem/medium/Problem55.java
@@ -1,7 +1,10 @@
 package problem.medium;
 
-import java.util.List;
 import problem.medium.resources.Customer2;
+import problem.medium.resources.Order;
+
+import java.util.Comparator;
+import java.util.List;
 
 public class Problem55 {
 
@@ -13,7 +16,13 @@ public class Problem55 {
      * @return 'Bread' 제품을 가장 많이 주문한 고객의 이름
      */
     public static String getCustomerWhoOrderedMostBread(List<Customer2> customers) {
-        // 여기에 코드 작성
-        return "";
+        return customers.stream()
+            .max(Comparator.comparing(customer -> customer.getOrders().stream()
+                .filter(order -> order.getProduct().equals("Bread"))
+                .map(Order::getQuantity)
+                .mapToInt(Integer::intValue)
+                .sum()))
+            .map(Customer2::getName)
+            .orElse("");
     }
 }
