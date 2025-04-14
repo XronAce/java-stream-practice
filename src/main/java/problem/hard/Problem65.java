bad1538d52147ba3e61c48b6d909902972f1ceff
Donggi Yang <xronace@gmail.com>
2025-04-14 17:05:40 +0900

    feat: Add Problem65 solution

--- src/main/java/problem/hard/Problem65.java
+++ src/main/java/problem/hard/Problem65.java
@@ -1,6 +1,9 @@
 package problem.hard;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Problem65 {
 
@@ -12,7 +15,14 @@ public class Problem65 {
      * @return 'a' 문자를 가장 많이 포함한 문자열, 없으면 빈 문자열 반환
      */
     public static String findStringWithMostAs(List<String> strings) {
-        // 여기에 코드 작성
-        return "";
+        return strings.stream()
+            .collect(Collectors.toMap(
+                s -> s,
+                s -> Arrays.stream(s.split("")).filter(c -> c.equals("a")).count()
+            ))
+            .entrySet().stream()
+            .max(Map.Entry.comparingByValue())
+            .map(Map.Entry::getKey)
+            .orElse("");
     }
 }
